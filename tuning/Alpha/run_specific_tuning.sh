561d85b34bae315c26aeb95d2217c810b6c44ae5
mrabie9 <md.rabie98@gmail.com>
2026-02-02 11:41:27 +0000

    hpt for lambda_det

--- tuning/Alpha/run_specific_tuning.sh
+++ tuning/Alpha/run_specific_tuning.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# python3 tuning/Alpha/tune_bcl.py --config configs/models/bcl_dual.yaml #--dry-run
 # python3 tuning/Alpha/tune_ctn.py --config configs/models/ctn.yaml #--dry-run
-python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml #--dry-run
-python3 tuning/Alpha/tune_icarl.py --config configs/models/icarl.yaml #--dry-run
-# python3 tuning/Alpha/tune_lwf.py --config configs/models/lwf.yaml #--dry-run
+# python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml #--dry-run
+# python3 tuning/Alpha/tune_icarl.py --config configs/models/icarl.yaml #--dry-run
+python3 tuning/Alpha/tune_er_ring.py --config configs/models/er_ring.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_eralg4.py --config configs/models/eralg4.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_ewc.py --config configs/models/ewc.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_si.py --config configs/models/si.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_bcl.py --config configs/models/bcl_dual.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_lwf.py --config configs/models/lwf.yaml --lr-first #--dry-run
+python3 tuning/Alpha/tune_rwalk.py --config configs/models/rwalk.yaml --lr-first #--dry-run
 # python3 tuning/Alpha/tune_ucl.py --config configs/models/ucl.yaml --lr-first #--dry-run
