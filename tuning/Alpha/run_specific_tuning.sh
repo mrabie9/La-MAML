9988a91cca803c02e9cd676fdb0963726b2d01ba
md.rabie98@gmail.com <wsmr11@LNX-ELKK-1.lunet.lboro.ac.uk>
2026-02-03 15:15:24 +0000

    servers split

--- tuning/Alpha/run_specific_tuning.sh
+++ tuning/Alpha/run_specific_tuning.sh
@@ -2,10 +2,10 @@
 # python3 tuning/Alpha/tune_ctn.py --config configs/models/ctn.yaml #--dry-run
 # python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml #--dry-run
 # python3 tuning/Alpha/tune_icarl.py --config configs/models/icarl.yaml #--dry-run
-python3 tuning/Alpha/tune_er_ring.py --config configs/models/er_ring.yaml --lr-first #--dry-run
-python3 tuning/Alpha/tune_eralg4.py --config configs/models/eralg4.yaml --lr-first #--dry-run
-python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --lr-first #--dry-run
-python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --lr-first #--dry-run
+# python3 tuning/Alpha/tune_er_ring.py --config configs/models/er_ring.yaml --lr-first #--dry-run
+# python3 tuning/Alpha/tune_eralg4.py --config configs/models/eralg4.yaml --lr-first #--dry-run
+# python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --lr-first #--dry-run
+# python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --lr-first #--dry-run
 python3 tuning/Alpha/tune_ewc.py --config configs/models/ewc.yaml --lr-first #--dry-run
 python3 tuning/Alpha/tune_si.py --config configs/models/si.yaml --lr-first #--dry-run
 python3 tuning/Alpha/tune_bcl.py --config configs/models/bcl_dual.yaml --lr-first #--dry-run
--- tuning/Bravo/run_specific_tuning.sh
+++ tuning/Bravo/run_specific_tuning.sh
@@ -6,6 +6,12 @@
 # python3 tuning/Bravo/tune_si.py --config configs/models/si.yaml --lr-first 
 # python3 tuning/Bravo/tune_rwalk.py --config configs/models/rwalk.yaml --lr-first 
 
-python3 tuning/Bravo/tune_smaml.py --config configs/models/smaml.yaml --tune-only "opt_wt,opt_lr,alpha_init" 
-python3 tuning/Bravo/tune_la-er.py --config configs/models/la-er.yaml 
+# python3 tuning/Bravo/tune_smaml.py --config configs/models/smaml.yaml --tune-only "opt_wt,opt_lr,alpha_init" 
+# python3 tuning/Bravo/tune_la-er.py --config configs/models/la-er.yaml 
 # python3 tuning/Bravo/tune_hat.py --config configs/models/hat.yaml --lr-first 
+
+python3 tuning/Bravo/tune_ewc.py --config configs/models/ewc.yaml --lr-first #--dry-run
+python3 tuning/Bravo/tune_si.py --config configs/models/si.yaml --lr-first #--dry-run
+python3 tuning/Bravo/tune_bcl.py --config configs/models/bcl_dual.yaml --lr-first #--dry-run
+python3 tuning/Bravo/tune_lwf.py --config configs/models/lwf.yaml --lr-first #--dry-run
+python3 tuning/Bravo/tune_rwalk.py --config configs/models/rwalk.yaml --lr-first #--dry-run
