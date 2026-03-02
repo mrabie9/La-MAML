43b0b96b658b5d3bd45be2d7812d0fe67e29eb41
mrabie9 <md.rabie98@gmail.com>
2026-03-02 11:22:31 +0000

    updated some tuning params

--- tuning/Alpha/run_specific_tuning.sh
+++ tuning/Alpha/run_specific_tuning.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# python3 tuning/Alpha/tune_er_ring.py --config configs/models/er_ring.yaml --lr-first #--dry-run
-# python3 tuning/Alpha/tune_eralg4.py --config configs/models/eralg4.yaml --lr-first  #--dry-run
-# python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --lr-first #--dry-run
-# python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --lr-first #--dry-run
-# python3 tuning/Alpha/tune_icarl.py --config configs/models/icarl.yaml --lr-first #--dry-run
-python3 tuning/Alpha/tune_ucl.py --config configs/models/ucl.yaml --lr-first #--dry-run
-# python3 tuning/Alpha/tune_lamaml.py --config configs/models/lamaml.yaml --tune-only "opt_lr,alpha_init" #--dry-run
+# python3 tuning/Alpha/tune_er_ring.py --config configs/models/er_ring.yaml --hierarchical --dry-run
+# python3 tuning/Alpha/tune_eralg4.py --config configs/models/eralg4.yaml --hierarchical  --dry-run
+python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_icarl.py --config configs/models/icarl.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_ucl.py --config configs/models/ucl.yaml --hierarchical --dry-run
+# python3 tuning/Alpha/tune_lamaml.py --config configs/models/lamaml.yaml --hierarchical --dry-run
+
+python3 tuning/Alpha/tune_bcl.py --config configs/models/bcl.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_ewc.py --config configs/models/ewc.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_si.py --config configs/models/si.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_lwf.py --config configs/models/lwf.yaml --hierarchical --dry-run
+python3 tuning/Alpha/tune_rwalk.py --config configs/models/rwalk.yaml --hierarchical --dry-run
