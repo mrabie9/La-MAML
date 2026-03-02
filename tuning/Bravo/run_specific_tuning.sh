2f00e7b4ddea88c50b263cc41ea65464a58b0ad4
mrabie9 <md.rabie98@gmail..com>
2026-03-02 11:12:39 +0000

    updated tuning

--- tuning/Bravo/run_specific_tuning.sh
+++ tuning/Bravo/run_specific_tuning.sh
@@ -9,20 +9,21 @@
 # python3 tuning/Bravo/tune_smaml.py --config configs/models/smaml.yaml --tune-only "opt_wt,opt_lr,alpha_init" 
 # python3 tuning/Bravo/tune_hat.py --config configs/models/hat.yaml --lr-first 
 
-# python3 tuning/Bravo/tune_ewc.py --config configs/models/ewc.yaml --lr-first #--dry-run
-# python3 tuning/Bravo/tune_si.py --config configs/models/si.yaml --lr-first #--dry-run
-# python3 tuning/Bravo/tune_bcl.py --config configs/models/bcl_dual.yaml --lr-first #--dry-run
-# python3 tuning/Bravo/tune_lwf.py --config configs/models/lwf.yaml --lr-first #--dry-run
-# python3 tuning/Bravo/tune_rwalk.py --config configs/models/rwalk.yaml --lr-first #--dry-run
-# python3 tuning/Bravo/tune_la-er.py --config configs/models/la-er.yaml --lr-first --lr-key "opt_lr" #--dry-run
+# python3 tuning/Bravo/tune_ewc.py --config configs/models/ewc.yaml --hierarchical #--dry-run
+# python3 tuning/Bravo/tune_si.py --config configs/models/si.yaml --hierarchical #--dry-run
+# python3 tuning/Bravo/tune_bcl.py --config configs/models/bcl_dual.yaml --hierarchical #--dry-run
+# python3 tuning/Bravo/tune_lwf.py --config configs/models/lwf.yaml --hierarchical #--dry-run
+# python3 tuning/Bravo/tune_rwalk.py --config configs/models/rwalk.yaml --hierarchical #--dry-run
+python3 tuning/Bravo/tune_la-er.py --config configs/models/la-er.yaml --hierarchical #--dry-run
 
-# python3 tuning/Bravo/tune_ctn.py --config configs/models/ctn.yaml --tune-only "ctx_lr, memory_strength" --hierarchical
-python3 tuning/Bravo/tune_cmaml.py --config configs/models/cmaml.yaml --hierarchical
-python3 tuning/Bravo/tune_smaml.py --config configs/models/smaml.yaml --hierarchical
-python3 tuning/Bravo/tune_hat.py --config configs/models/hat.yaml --hierarchical
+python3 tuning/Bravo/tune_ctn.py --config configs/models/ctn.yaml --hierarchical
+# python3 tuning/Bravo/tune_cmaml.py --config configs/models/cmaml.yaml --hierarchical
+# python3 tuning/Bravo/tune_smaml.py --config configs/models/smaml.yaml --hierarchical
+# python3 tuning/Bravo/tune_hat.py --config configs/models/hat.yaml --hierarchical
 python3 tuning/Bravo/tune_packnet.py --config configs/models/packnet.yaml --hierarchical
-python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --hierarchical
-python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --hierarchical
-python3 tuning/Bravo/tune_ucl.py --config configs/models/ucl.yaml --tune-only "beta"
+# python3 tuning/Alpha/tune_agem.py --config configs/models/agem.yaml --hierarchical
+# python3 tuning/Alpha/tune_gem.py --config configs/models/gem.yaml --hierarchical
+python3 tuning/Bravo/tune_ucl.py --config configs/models/ucl.yaml --hierarchical
+
 
 #--dry-run
